6f7e7427b6a194e684338241a7079f07ebec6bf4
Rebecca Tseng <165713650+retseng@users.noreply.github.com>
2024-06-01 14:21:36 -0700

    Update report5_bug.java

--- report5_bug.java
+++ report5_bug.java
@@ -9,41 +9,16 @@ class ListExamples {
   // the StringChecker returns true, and not the elements that return false, in
   // the same order they appeared in the input list;
   static List<String> filter(List<String> list, StringChecker sc) {
-    List<String> result = new ArrayList<>();
     for(String s: list) {
       if(sc.checkString(s)) {
+        List<String> result = new ArrayList<>();
         result.add(s);
       }
+      return;
     }
     return result;
   }
 
-
-  // Takes two sorted list of strings (so "a" appears before "b" and so on),
-  // and return a new list that has all the strings in both list in sorted order.
-  static List<String> merge(List<String> list1, List<String> list2) {
-    List<String> result = new ArrayList<>();
-    int index1 = 0, index2 = 0;
-    while(index1 < list1.size() && index2 < list2.size()) {
-      if(list1.get(index1).compareTo(list2.get(index2)) < 0) {
-        result.add(list1.get(index1));
-        index1 += 1;
-      }
-      else {
-        result.add(list2.get(index2));
-        index2 += 1;
-      }
-    }
-    while(index1 < list1.size()) {
-      result.add(list1.get(index1));
-      index1 += 1;
-    }
-    while(index2 < list2.size()) {
-      result.add(list2.get(index2));
-      index2 += 1;
-    }
-    return result;
-  }
 
 
 }
