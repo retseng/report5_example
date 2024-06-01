925ee781a2af6a8ff0f80f3972c5ea2ea8785738
Rebecca Tseng <165713650+retseng@users.noreply.github.com>
2024-06-01 15:25:07 -0700

    Update ListExamples.java

--- ListExamples.java
+++ ListExamples.java
@@ -8,16 +8,19 @@ class ListExamples {
   // Returns a new list that has all the elements of the input list for which
   // the StringChecker returns true, and not the elements that return false, in
   // the same order they appeared in the input list;
-  static List<String> filter(List<String> list, StringChecker sc) {
-    for(String s: list) {
-      if(sc.checkString(s)) {
-        List<String> result = new ArrayList<>();
-        result.add(s);
+         import java.util.ArrayList;
+         import java.util.List;
+         interface StringChecker { boolean checkString(String s); }
+         class ListExamples {
+         static List<String> filter(List<String> list, StringChecker sc) {
+         List<String> result = new ArrayList<>();
+         for(String s: list) {
+            if(sc.checkString(s)) {
+              result.add(s);
+            }
+        }
         return result;
       }
-    }
-    return list;
-  }
 
 
   // Takes two sorted list of strings (so "a" appears before "b" and so on),
